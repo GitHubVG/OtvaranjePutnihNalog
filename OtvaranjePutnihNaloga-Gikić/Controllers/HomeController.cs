75790b07562ab95c30d48f20bf086a266fd2d08e
GitHubVG <vladimirgikic@yahoo.com>
2015-04-16 15:53:50 +0200

    'Commit'

--- OtvaranjePutnihNaloga-Gikić/Controllers/HomeController.cs
+++ OtvaranjePutnihNaloga-Gikić/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using OtvaranjePutnihNaloga_Gikić.Models.Baza_zaposlenika;
+﻿using OtvaranjePutnihNaloga_Gikić.Models;
+using OtvaranjePutnihNaloga_Gikić.Models.Baza_zaposlenika;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -24,50 +25,9 @@ namespace OtvaranjePutnihNaloga_Gikić.Controllers
         {
             //brisanje nedovršenih putnih naloga iz baze 
             //(ako korisnik popuni putni nalog a ne ispuni podatke o smjestaju ili privatnom vozilu (ako postoji))
-
-            var test = db.PutniNalog.OrderByDescending(x => x.ID).First().PrijevoznaSredstva.IDTipPrijevoznogSredstva;
-            var test2 = db.PutniNalog.OrderByDescending(x => x.ID).First().Smještaj;
-
-            if (db.PutniNalog.Any(x=>x.Smještaj) || db.PutniNalog.Any(x => x.PrijevoznaSredstva.IDTipPrijevoznogSredstva==2))
-            {
-                var IDzadnjiPutniNalog = db.PutniNalog.OrderByDescending(x => x.ID).First().ID;
-                List<int> IDputnihNalogaSaSmjestajem = new List<int>();
-                List<int> listaPrijevoznihSredstava = new List<int>();
-
-                foreach (var id in db.PutniNalog.Where(x=>x.Smještaj))
-                {
-                    IDputnihNalogaSaSmjestajem.Add(id.ID);
-                }
-                var smjestaji = db.Smejstaj;
-
-                foreach(var id in db.PutniNalog.Where(x=>x.PrijevoznaSredstva.IDTipPrijevoznogSredstva==2))
-                {
-                    listaPrijevoznihSredstava.Add(id.ID);
-                }
-                                                
-                
-                for (int i = 0; i < IDputnihNalogaSaSmjestajem.Count;i++ )
-                {
-                    int placeholder= IDputnihNalogaSaSmjestajem[i];
-                    if (smjestaji.Any(x => x.IDPutnogNaloga != placeholder))
-                    {
-                        db.PutniNalog.Remove(db.PutniNalog.Single(x => x.ID == placeholder));
-
-                        //mora se obirsati i privatno vozilo ako postoji zbog dependencija u bazi (foreign key)
-                        if(db.PrivatnoVozilo.Any(x=>x.IDPutnogNaloga==placeholder))
-                        {
-                            db.PrivatnoVozilo.Remove(db.PrivatnoVozilo.Single(x => x.IDPutnogNaloga == placeholder));
-
-                        }
-
-
-                        db.SaveChanges();
-                    }
-
-                }
-                                               
-            }
-
+            ProvjeraPodataka provjera = new ProvjeraPodataka();
+            provjera.ProvjeraIspunjenogNaloga();
+            
 
             provjeraSmjestaj = false;
             provjeraPrijevozPrivatnimVozilom = false;
---
+++ OtvaranjePutnihNaloga-Gikić/Models/ProvjeraPodataka.cs
@@ -0,0 +1,61 @@
+﻿using OtvaranjePutnihNaloga_Gikić.Models.Baza_zaposlenika;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OtvaranjePutnihNaloga_Gikić.Models
+{
+    public class ProvjeraPodataka
+    {
+        public void ProvjeraIspunjenogNaloga()
+        {
+            PutniNaloziEntities db = new PutniNaloziEntities();
+
+           // var test = db.PutniNalog.OrderByDescending(x => x.ID).First().PrijevoznaSredstva.IDTipPrijevoznogSredstva;
+           // var test2 = db.PutniNalog.OrderByDescending(x => x.ID).First().Smještaj;
+
+            if (db.PutniNalog.Any(x => x.Smještaj) || db.PutniNalog.Any(x => x.PrijevoznaSredstva.IDTipPrijevoznogSredstva == 2))
+            {
+                var IDzadnjiPutniNalog = db.PutniNalog.OrderByDescending(x => x.ID).First().ID;
+                List<int> IDputnihNalogaSaSmjestajem = new List<int>();
+                List<int> listaPrijevoznihSredstava = new List<int>();
+
+                foreach (var id in db.PutniNalog.Where(x => x.Smještaj))
+                {
+                    IDputnihNalogaSaSmjestajem.Add(id.ID);
+                }
+                var smjestaji = db.Smejstaj;
+
+                foreach (var id in db.PutniNalog.Where(x => x.PrijevoznaSredstva.IDTipPrijevoznogSredstva == 2))
+                {
+                    listaPrijevoznihSredstava.Add(id.ID);
+                }
+
+
+                for (int i = 0; i < IDputnihNalogaSaSmjestajem.Count; i++)
+                {
+                    int placeholder = IDputnihNalogaSaSmjestajem[i];
+                    if (smjestaji.Any(x => x.IDPutnogNaloga != placeholder))
+                    {
+                        db.PutniNalog.Remove(db.PutniNalog.Single(x => x.ID == placeholder));
+
+                        //mora se obirsati i privatno vozilo ako postoji zbog dependencija u bazi (foreign key)
+                        if (db.PrivatnoVozilo.Any(x => x.IDPutnogNaloga == placeholder))
+                        {
+                            db.PrivatnoVozilo.Remove(db.PrivatnoVozilo.Single(x => x.IDPutnogNaloga == placeholder));
+
+                        }
+
+
+                        db.SaveChanges();
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+}
